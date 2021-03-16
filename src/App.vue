079703a0770e2339cc810970168f53b8e573e70f
fabienD31 <fabien.desnoues31@gmail.com>
2021-03-16 12:16:01 +0100

    ajout getters

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,13 @@
     </div>
     <div class="card">
       <div class="card-header d-flex flex-row justify-content-between">
-        <span>Todo list</span>
+        <span>Todo list</span><span>{{ todosLength }}</span>
       </div>
       <div class="card-body">
         <ul class="list-group">
           <li
+            v-for="todo in todos"
+            :key="todo.name"
             class="list-group-item d-flex flex-row justify-content-between align-items-center list-group-item-action"
           >
             <span>{{ todo.name }}</span>
@@ -27,12 +29,27 @@
   </div>
 </template>
 <script>
+import { mapState } from "vuex"; //Pour utiliser le state en version raccourci
+import { mapGetters } from "vuex"; // Pour utiliser les getters en version raccourcis
 export default {
   name: "app",
   data() {
     return {
       todo: "",
     };
+  },
+  // computed: {
+  //   todos() {
+  //     return this.$store.state.todos;
+  //   },
+  //   todosLength() {
+  //     return this.$store.getters.todosLenght;
+  //   },
+  // },
+  //version raccourci d'utilisation du store + getters
+  computed: {
+    ...mapState(["todos"]),
+    ...mapGetters(["todosLength"]),
   },
 };
 </script>
---
+++ src/store.js
@@ -0,0 +1,26 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    todos: [
+    {
+      name: 'eat pizza',
+      done: false
+    },
+    {
+      name: 'work',
+      done: false,
+    }
+  ]
+  },
+  getters: {
+    todosLength(state) {
+      return state.todos.length;
+    }
+  }
+});
+
+export default store;
