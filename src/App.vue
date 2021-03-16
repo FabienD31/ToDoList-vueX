541a1e30786ad42d9f088c12c636dcf357deadf0
fabienD31 <fabien.desnoues31@gmail.com>
2021-03-16 10:58:07 +0100

    mise en place graphique

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container p-5">
+    <h4>Ajouter une todo</h4>
+    <hr class="my-4" />
+    <div class="d-flex mb-4">
+      <input v-model="todo" type="text" class="form-control mr-5" />
+      <button class="btn btn-danger">Ajouter</button>
+    </div>
+    <div class="card">
+      <div class="card-header d-flex flex-row justify-content-between">
+        <span>Todo list</span>
+      </div>
+      <div class="card-body">
+        <ul class="list-group">
+          <li
+            class="list-group-item d-flex flex-row justify-content-between align-items-center list-group-item-action"
+          >
+            <span>{{ todo.name }}</span>
+            <span>
+              <input class="mx-3" type="checkbox" />
+              <button class="btn btn-sm btn-danger">delete</button>
+            </span>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "app",
+  data() {
+    return {
+      todo: "",
+    };
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
