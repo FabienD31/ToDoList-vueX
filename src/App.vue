57044aeeb88c7d71dbb49a48f1488418fb7e773b
fabienD31 <fabien.desnoues31@gmail.com>
2021-03-22 10:50:39 +0100

    muttation avec et sans raccourcis

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,13 @@
     <hr class="my-4" />
     <div class="d-flex mb-4">
       <input v-model="todo" type="text" class="form-control mr-5" />
-      <button class="btn btn-danger">Ajouter</button>
+      <button
+        @click="addTodo({ name: todo, done: false })"
+        class="btn btn-danger"
+      >
+        <!--paramettre addTodo qui correspond au payload pour la version raccourcis-->
+        Ajouter
+      </button>
     </div>
     <div class="card">
       <div class="card-header d-flex flex-row justify-content-between">
@@ -13,14 +19,17 @@
       <div class="card-body">
         <ul class="list-group">
           <li
-            v-for="todo in todos"
+            @click="toggleTodo(index)"
+            v-for="(todo, index) in todos"
             :key="todo.name"
             class="list-group-item d-flex flex-row justify-content-between align-items-center list-group-item-action"
           >
             <span>{{ todo.name }}</span>
             <span>
-              <input class="mx-3" type="checkbox" />
-              <button class="btn btn-sm btn-danger">delete</button>
+              <input :checked="todo.done" class="mx-3" type="checkbox" />
+              <button @click="deleteTodo(index)" class="btn btn-sm btn-danger">
+                delete
+              </button>
             </span>
           </li>
         </ul>
@@ -31,6 +40,7 @@
 <script>
 import { mapState } from "vuex"; //Pour utiliser le state en version raccourci
 import { mapGetters } from "vuex"; // Pour utiliser les getters en version raccourcis
+import { mapMutations } from "vuex"; // Pour utiliser les mutations en version raccourcis
 export default {
   name: "app",
   data() {
@@ -38,6 +48,7 @@ export default {
       todo: "",
     };
   },
+  //Version complete store + getter
   // computed: {
   //   todos() {
   //     return this.$store.state.todos;
@@ -50,6 +61,23 @@ export default {
   computed: {
     ...mapState(["todos"]),
     ...mapGetters(["todosLength"]),
+  },
+
+  //Version complete mutation
+  // methods: {
+  //   addTodo() {
+  //     this.$store.commit("addTodo", { name: this.todo, done: false });
+  //   },
+  //   toggleTodo(index) {
+  //     this.$store.commit("toggleTodo", index);
+  //   },
+  //   deleteTodo(index) {
+  //     this.$store.commit("deleteTodo", index);
+  //   },
+  // },
+  //Version raccourcis mutation
+  methods: {
+    ...mapMutations(["addTodo", "toogleTodo", "deleteTodo"]),
   },
 };
 </script>
--- src/store.js
+++ src/store.js
@@ -20,6 +20,17 @@ const store = new Vuex.Store({
     todosLength(state) {
       return state.todos.length;
     }
+  },
+  mutations: {
+    addTodo(state, payload) {
+      state.todos = [...state.todos, payload ];
+    },
+    toggleTodo(state, payload) {
+      state.todos[payload].done = !state.todos[payload].done
+    },
+    deleteTodo(state, payload) {
+      state.todos.splice(payload, 1)
+    }
   }
 });
 
